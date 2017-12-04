a10fa976dcf08c748f95961da0d3e7bddbe990f3
Percy Soliz <psolizrodriguez@luc.edu>
2017-12-04 17:59:42 -0600

    Changes on index

--- src/main/resources/public/scripts/index.js
+++ src/main/resources/public/scripts/index.js
@@ -60,10 +60,6 @@ function hideDialogBlockDialog() {
 	$("#dialog-loading").dialog("close");
 
 }
-function previewFile(event) {
-	var profileImage = document.getElementById('profileImage');
-	profileImage.src = URL.createObjectURL(event.target.files[0]);
-};
 function cancelSave() {
 	list.show();
 	form.hide();
@@ -77,58 +73,6 @@ function refreshContactsList() {
 		drawSearchResults(data);
 	});
 
-}
-function milisToDate(time) {
-	var time = new Date().getTime();
-	var date = new Date(time);
-	return date.toString();
-}
-
-function loadDataToForm(varUrl) {
-	list.hide();
-	form.show();
-	showDialogBlockDialog("Loading Data from Server");
-	if (varUrl == 0) {
-		document.getElementById('contactId').value = "0";
-		document.getElementById('name').value = "";
-		document.getElementById('company').value = "";
-		document.getElementById('profileImage').src = "img/testProfile.jpg";
-		document.getElementById('email').value = "";
-		document.getElementById('birthdate').value = "";
-		document.getElementById('personalPhoneNumber').value = "";
-		document.getElementById('workPhoneNumber').value = "";
-		document.getElementById('addressId').value = "0";
-		document.getElementById('street').value = "";
-		document.getElementById('unit').value = "";
-		document.getElementById('city').value = "";
-		document.getElementById('state').value = "";
-		document.getElementById('zip').value = "";
-		$("#save").attr("onclick", "saveContact(0)");
-		hideDialogBlockDialog();
-	} else {
-		console.log(varUrl);
-		varUrl = simplifyURL(varUrl);
-		console.log(varUrl);
-
-		$.getJSON(varUrl).done(function(data) {
-			document.getElementById('contactId').value = data.contactId;
-			document.getElementById('name').value = data.name;
-			document.getElementById('company').value = data.company;
-			document.getElementById('profileImage').src = data._links['view'].href;
-			document.getElementById('email').value = data.email;
-			document.getElementById('birthdate').value = formatDate(milisToDate(data.birthdate));
-			document.getElementById('personalPhoneNumber').value = data.personalPhoneNumber;
-			document.getElementById('workPhoneNumber').value = data.workPhoneNumber;
-			document.getElementById('addressId').value = data.address.addressId;
-			document.getElementById('street').value = data.address.street;
-			document.getElementById('unit').value = data.address.unit;
-			document.getElementById('city').value = data.address.city;
-			document.getElementById('state').value = data.address.state;
-			document.getElementById('zip').value = data.address.zip;
-			$("#save").attr("onclick", "saveContact('" + data._links['save'].href + "')");
-			hideDialogBlockDialog();
-		});
-	}
 }
 $(function() {
 	var availableProducts = [ "FitBit", "Bluetooth Headphones", "Laptop Bag", "The Odyssey" ];
@@ -260,49 +204,6 @@ function loadInventoryDetailsToReviewOrder(selfLink) {
 		// hideDialogBlockDialog();
 	});
 }
-
-function simplifyURL(varUrl) {
-	var pathArray = varUrl.split('/');
-	if (pathArray[pathArray.length - 2].includes(":")) {
-		varUrl = pathArray[pathArray.length - 1];
-	} else {
-		varUrl = pathArray[pathArray.length - 2] + '/' + pathArray[pathArray.length - 1];
-	}
-	return varUrl;
-}
-
-function deleteContact(varUrl) {
-	varUrl = simplifyURL(varUrl);
-	console.log(varUrl);
-	showConfirmDialog("Are you sure?", function() {
-		showDialogBlockDialog("Deleting");
-		$.ajax({
-			url : varUrl,
-			method : 'DELETE',
-			type : 'DELETE',
-			success : function(result) {
-				console.log(result);
-				hideDialogBlockDialog();
-				showDialog("Contact Deleted", function() {
-					refreshContactsList();
-				});
-			},
-			error : function(request, msg, error) {
-				console.log(error);
-			}
-		});
-	});
-}
-
-function getBase64Image(img) {
-	var canvas = document.createElement("canvas");
-	canvas.width = img.width;
-	canvas.height = img.height;
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0, img.width, img.height);
-	var dataURL = canvas.toDataURL("image/png");
-	return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
 function loadValuesIntoOrder(inventoryId) {
 	var order = {
 		"customerId" : 1,
@@ -337,52 +238,9 @@ function placeOrder(submitURL, inventoryId) {
 	console.log(order);
 	$.post("order", JSON.stringify(order), "json").done(function(data) {
 		hideDialogBlockDialog();
-		showDialog("Contact Saved", function() {
+		showDialog("Order Placed", function() {
 			refreshContactsList();
 		});
 
 	});
-
-	// submit
-}
-function saveContact(varUrl) {
-	showDialogBlockDialog("Saving");
-	if (varUrl == 0) {
-
-		$.post("inventory", JSON.stringify(loadFormIntoContact()), "json").done(function(data) {
-			hideDialogBlockDialog();
-			showDialog("Contact Saved", function() {
-				refreshContactsList();
-			});
-
-		});
-	} else {
-		varUrl = simplifyURL(varUrl);
-		$.ajax({
-			url : varUrl,
-			method : "PUT",
-			type : "PUT",
-			data : JSON.stringify(loadFormIntoContact()),
-			success : function(result) {
-				console.log(result);
-				hideDialogBlockDialog();
-				showDialog("Contact Saved", function() {
-					refreshContactsList();
-				});
-			},
-			error : function(request, msg, error) {
-				console.log(error);
-			}
-		});
-	}
-}
-function formatDate(date) {
-	var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
-
-	if (month.length < 2)
-		month = '0' + month;
-	if (day.length < 2)
-		day = '0' + day;
-
-	return [ year, month, day ].join('-');
 }
